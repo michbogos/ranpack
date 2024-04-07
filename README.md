9a725b303eae5136be11496cedbfce4a7f721b00
michbogos <bogostamicheal@gmail.com>
2024-04-07 20:50:10 +0200

    Add command line arguments and gif generation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Random circle packing
 
-![A koala made of circles](https://github.com/michbogos/ranpack/blob/main/res.png?raw=true)
+![A koala made of circles](https://github.com/michbogos/ranpack/blob/main/res.gif?raw=true)
 
 Uses randdom packing to generate an stylized image.
--- ranpack.py
+++ ranpack.py
@@ -2,26 +2,45 @@ from PIL import Image, ImageDraw
 import random
 import math
 from tqdm import trange
+import argparse
 
-GRID_SIZE = 100
+parser = argparse.ArgumentParser(
+                    prog='Ranpack',
+                    description='Creates a stylized image from a base image',
+                    epilog='Ranpack packs circles randomly')
 
-img = Image.open("koala.webp")
+parser.add_argument("input")
+parser.add_argument("output")
+parser.add_argument("-n", "--number")
+parser.add_argument("-r", "--radius")
+
+args = parser.parse_args()
+
+N = int(args.number) if args.number else 100000
+RADIUS = int(args.radius) if args.radius else 30
+images = []
+
+print(args)
+
+img = Image.open(args.input)
 res = Image.new("RGB", img.size, (0, 0, 0))
+
+GRID_SIZE = int(min(img.size)/RADIUS/2)
 
 circles = [[[] for j in range(GRID_SIZE)] for i in range(GRID_SIZE)]
 
 x = random.randint(0, img.size[0]-1)
 y = random.randint(0, img.size[1]-1)
-r = random.randint(5, 30)
+r = 1
 
 circles[int(x/img.size[0]*GRID_SIZE)][int(y/img.size[1]*GRID_SIZE)].append(((x, y), r))
 
 draw = ImageDraw.Draw(res)
 
-for i in (t:=trange(200000)):
+for iter in (t:=trange(N)):
     x = random.randint(0, img.size[0]-1)
     y = random.randint(0, img.size[1]-1)
-    r = 10
+    r = RADIUS
     checks = 0
     i = min(max(1, int(x/img.size[0]*GRID_SIZE)), GRID_SIZE-2)
     j = min(max(1, int(y/img.size[1]*GRID_SIZE)), GRID_SIZE-2)
@@ -40,6 +59,12 @@ for i in (t:=trange(200000)):
         circles[int(x/img.size[0]*GRID_SIZE)][int(y/img.size[1]*GRID_SIZE)].append(((x, y), r))
         draw.ellipse((x-r, y-r, x+r, y+r), (img.getpixel((x, y))))
 
-res.save("res.png")
+        if iter%1000==0:
+            images.append(res.copy())
 
+
+res.save(args.output)
 res.show()
+
+gif = Image.new("RGBA", res.size)
+gif.save("res.gif", save_all=True, append_images=images)
